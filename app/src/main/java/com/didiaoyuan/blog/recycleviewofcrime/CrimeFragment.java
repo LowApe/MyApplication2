abd48bab8bb7b19a0ff0722b63ad79c5349e3de4
LowApe <didiaoyuan@gmail.com>
2017-09-14 18:33:34 +0800

    删除不必要的导包 *添加更新的索引，准确更新

--- app/src/main/java/com/didiaoyuan/blog/recycleviewofcrime/CrimeFragment.java
+++ app/src/main/java/com/didiaoyuan/blog/recycleviewofcrime/CrimeFragment.java
@@ -2,10 +2,9 @@ package com.didiaoyuan.blog.recycleviewofcrime;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
--- app/src/main/java/com/didiaoyuan/blog/recycleviewofcrime/CrimeListFragment.java
+++ app/src/main/java/com/didiaoyuan/blog/recycleviewofcrime/CrimeListFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,7 +39,8 @@ public class CrimeListFragment extends Fragment {
             mCrimeAdapter=new CrimeAdapter(crimes);
             mRecyclerView.setAdapter(mCrimeAdapter);
         }else{
-            mCrimeAdapter.notifyItemChanged(mIndexNotific);
+//            索引减1
+            mCrimeAdapter.notifyItemChanged(mIndexNotific-1);
         }
     }
 
