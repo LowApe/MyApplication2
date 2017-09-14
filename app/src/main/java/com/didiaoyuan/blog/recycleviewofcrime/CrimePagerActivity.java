23cf3f1c76d911404bda3c5f2c727010caeb84fd
LowApe <didiaoyuan@gmail.com>
2017-09-14 11:10:47 +0800

    1.定义变量和初始化组件 2.添加ViewPager的适配器，使用代理FragmentStatePagerAdapter，并传入fm 3.添加newIntent方法，实现点击跳转到本页面，停用MainActivity

--- app/src/main/java/com/didiaoyuan/blog/recycleviewofcrime/CrimePagerActivity.java
+++ app/src/main/java/com/didiaoyuan/blog/recycleviewofcrime/CrimePagerActivity.java
@@ -1,14 +1,47 @@
 package com.didiaoyuan.blog.recycleviewofcrime;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CrimePagerActivity extends FragmentActivity {
+import java.util.List;
+import java.util.UUID;
 
+public class CrimePagerActivity extends FragmentActivity {
+//声明变量
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+//        获取组件
+        mViewPager=findViewById(R.id.view_pager);
+        mCrimes=CrimeLab.get(this).getCrimes();
+        FragmentManager fm=getSupportFragmentManager();/*FragmentStatePagerAdapter需要fragmentManager*/
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime=mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId(),position);
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+    }
+//    方法
+    public Intent newIntent(Context packagecontext, UUID crimeID){
+        Intent i=new Intent(packagecontext,CrimePagerActivity.class);
+        i.putExtra("CrimeId",crimeID);
+        return i;
     }
 }
