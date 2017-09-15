ff30bd68294c5e083a58f51b001f78696ede657c
LowApe <didiaoyuan@gmail.com>
2017-09-15 13:56:02 +0800

    清理包

--- app/src/main/java/com/didiaoyuan/blog/recycleviewofcrime/DatePickerFragment.java
+++ app/src/main/java/com/didiaoyuan/blog/recycleviewofcrime/DatePickerFragment.java
@@ -1,7 +1,6 @@
 package com.didiaoyuan.blog.recycleviewofcrime;
 
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
