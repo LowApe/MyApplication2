67b58998b4d46b1f737194cd47e3f39b3ace8259
LowApe <didiaoyuan@gmail.com>
2017-09-15 11:03:43 +0800

    1.在 DatePickerFragment 中生成这个View 2.setView()方法添加视图

--- app/src/main/java/com/didiaoyuan/blog/recycleviewofcrime/DatePickerFragment.java
+++ app/src/main/java/com/didiaoyuan/blog/recycleviewofcrime/DatePickerFragment.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by Mr.Qu on 2017/9/15.
@@ -13,7 +15,10 @@ public class DatePickerFragment extends android.support.v4.app.DialogFragment{
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View  view= LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date,null);
         return new AlertDialog.Builder(getActivity())
+                .setView(view)
                 .setTitle("Date_Picker_title")
                 .setPositiveButton(android.R.string.ok,null)
                 .create();
