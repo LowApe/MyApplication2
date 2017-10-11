54ad2b599a04972f81d3560167fef515fa5363e5
LowApe <didiaoyuan@gmail.com>
2017-10-11 14:57:08 +0800

    给整个应用添加一个字段，更新数据库以及其他方法

--- app/src/main/java/com/didiaoyuan/blog/recycleviewofcrime/Crime.java
+++ app/src/main/java/com/didiaoyuan/blog/recycleviewofcrime/Crime.java
@@ -12,7 +12,7 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
-
+    private String mPeopleName;
     public Crime() {
 //        mId = UUID.randomUUID();
 //        mDate = new Date();
@@ -50,5 +50,13 @@ public class Crime {
 
     public void setSolved(boolean solved) {
         mSolved = solved;
+    }
+
+    public String getPeopleName() {
+        return mPeopleName;
+    }
+
+    public void setPeopleName(String peopleName) {
+        mPeopleName = peopleName;
     }
 }
