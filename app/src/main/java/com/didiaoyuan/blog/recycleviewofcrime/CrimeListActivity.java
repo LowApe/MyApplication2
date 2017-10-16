10e0b4b4b41293a99ae3237905665ae823bfa3f1
LowApe <didiaoyuan@gmail.com>
2017-10-16 13:19:29 +0800

    通过定义别名资源实现不同设备之间使用布局 1. 小于600dp 使用单版面 2. 大于600dp 使用双版面

--- app/src/main/java/com/didiaoyuan/blog/recycleviewofcrime/CrimeListActivity.java
+++ app/src/main/java/com/didiaoyuan/blog/recycleviewofcrime/CrimeListActivity.java
@@ -11,6 +11,7 @@ public class CrimeListActivity extends SimpleFragmentActivity {
 
     @Override
     protected int getLayoutResId() {
-        return R.layout.activity_twopane;
+//        return R.layout.activity_twopane;
+        return R.layout.activity_masterdetail;
     }
 }
